07452307d79aa3c0862da0929aebf17a35a9b3b6
fus3rx <amitgtx@gmail.com>
2017-01-12 00:21:23 +0600

    this is the alpha build of sales management with lots of warnings!

---
+++ app/src/main/java/com/example/shahr/salesmanagement/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shahr.salesmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginClick(View view) {
+    }
+
+    public void registerClick(View view) {
+    }
+}
